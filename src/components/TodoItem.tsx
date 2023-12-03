23c72d9720db1a93aa7737e29a22f5537c6d0377
Oops14 <ups14q@gmail.com>
2023-12-03 17:53:50 +0200

    edit dunc.

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,31 +1,69 @@
-import React, { ChangeEvent, MouseEventHandler, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 type TodoItemPropsType = {
     todoTitle?: string;
     id: string;
     complited: boolean;
-    deleteEl?: any;
-    isChecked: (e: ChangeEvent<HTMLInputElement> , id: string) => void ;
+    deleteEl: (id: string) => void;
+    isChecked: (e: ChangeEvent<HTMLInputElement>, id: string) => void;
+    editTodo: (id: string) => void;
+    editedItem: boolean;
+    setEditedItem: (editedItem: boolean) => void;
+    updateTodo: (id: string, newTitle: any) => void;
 };
 
 export const TodoItem = (props: TodoItemPropsType) => {
+    const [updatedTitle, setUpdatedTitle] = useState('');
+
+    // Remove element func.
     const removeElement = () => {
         props.deleteEl(props.id);
-    }
+    };
 
+    // Checked input func.
     const setChecked = (e: ChangeEvent<HTMLInputElement>) => {
         props.isChecked(e, props.id);
-    }
+    };
 
-    return (
-        <div className="todo-item">
+    const editTodoItem = () => {
+        props.editTodo(props.id);
+    };
+
+    return props.editedItem ? (
+        <div  className={"todo-item todo-item-edited"}>
+            <form className="edited-form" action="#">
+                <input type="text" placeholder={props.todoTitle} value={updatedTitle} onChange={(e) => {
+                    e.preventDefault();
+
+                    console.log(e.currentTarget.value);
+                    setUpdatedTitle(e.currentTarget.value);
+                }} />
+            </form>
+
+            <div>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a onClick={() => {
+                    props.updateTodo(props.id, updatedTitle);
+                }} className={"btn btn-edit"} href="#">
+                    Update
+                </a>
+            </div>
+        </div>
+    ) : (
+        <div className={props.complited ? "todo-item complited" : "todo-item"}>
             <h3 className={"todo-item-title"}>{props.todoTitle}</h3>
 
             <div>
-                <a  className={"btn btn-edit"} href="#">
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a onClick={editTodoItem} className={"btn btn-edit"} href="#">
                     Edit
                 </a>
-                <a onClick={removeElement} className={"btn btn-delete"} href="#">
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a
+                    onClick={removeElement}
+                    className={"btn btn-delete"}
+                    href="#"
+                >
                     Delete
                 </a>
                 <input
--- src/components/TodoWrapper.tsx
+++ src/components/TodoWrapper.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, MouseEventHandler, useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import {TodoForm} from "./TodoForm";
 import {TodoItem} from "./TodoItem";
 import {v4 as uuidv4} from "uuid";
@@ -7,24 +7,29 @@ type TodoItemType = {
     id: string;
     task: string;
     isComplited: boolean;
+    isEdited: boolean;
 }
 
 export const TodoWrapper = () => {
     const [todoItem, setTodoItem] = useState<Array<TodoItemType>>([]);
-
     React.useEffect(() => console.log("data", todoItem), [todoItem]);
 
+    const [editedItem, setEditedItem] = useState<boolean>(true);
+
+    // Add todo item to the list.
     const addTodo = (todo: any) => {
         setTodoItem([
             ...todoItem,
-            {id: uuidv4(), task: todo, isComplited: false},
+            {id: uuidv4(), task: todo, isComplited: false, isEdited: false},
         ]);
     };
 
+    // Remove todo item.
     const removeElement = (id: string) => {
         setTodoItem(todoItem.filter((todo: any) => todo.id !== id));
     };
 
+    // Set input as done.
     const isChecked = (e: ChangeEvent<HTMLInputElement>, id: string) => {
         let check = e.currentTarget.checked;
         let checkedTodo = todoItem.find(item => item.id === id);
@@ -35,9 +40,19 @@ export const TodoWrapper = () => {
         }
     }
 
+    const editTodo = (id: string) => {
+        let edited = todoItem.map(item => item.id === id ? {...item, isEdited: editedItem} : item);
+        setTodoItem(edited);
+    }
+
+    const updateTodo = (id: string, newTitle: any) => {
+        let updated = todoItem.map(item => item.id === id ? {...item, task: newTitle, isEdited: !editedItem} : item);
+        setTodoItem(updated);
+    }
+
     return (
         <div className={"todo-wrapper"}>
-            <TodoForm addTodo={addTodo}/>
+            <TodoForm  addTodo={addTodo}/>
 
             {todoItem.map((item) => {
                 return (
@@ -48,6 +63,10 @@ export const TodoWrapper = () => {
                         complited={item.isComplited}
                         deleteEl={removeElement}
                         isChecked={isChecked}
+                        editTodo={editTodo}
+                        editedItem={item.isEdited}
+                        setEditedItem={setEditedItem}
+                        updateTodo={updateTodo}
                     />
                 );
             })}
